90356a60f8b595b1840ca8c7d847589abbe1aeb0
Will Sheppard <sheppard.will@gmail.com>
2012-12-04 13:50:21 +0000

    Updated keypad module.

--- ecd_project/ip/keypad/keypad.vhdl
+++ ecd_project/ip/keypad/keypad.vhdl
@@ -13,30 +13,32 @@ entity keypad_entity is
          s1_reset        : in    std_logic;
          s1_read         : in    std_logic;
          s1_address      : in    std_logic_vector(1 downto 0);
-         s1_readdata     : out    std_logic_vector(31 downto 0)
+         s1_readdata     : out   std_logic_vector(31 downto 0)
 
        );
 end;
 
 
 architecture keypad_behaviour of keypad_entity is
-  signal cols_counter : unsigned(1 downto 0) := "00";
+  signal cols_counter  : unsigned(1 downto 0) := "00";
   signal internal_keys : std_logic_vector(15 downto 0) := "0000000000000000";
-  signal keystate : std_logic_vector(15 downto 0) := "0000000000000000";
-
+  signal keystate      : std_logic_vector(15 downto 0) := "0000000000000000";
+  signal keypad_clk : std_logic := '1';
+  signal keypad_clk_count : integer range 0 to 500000;
+  
 begin
 
   -- These are hardwired states I guess!
-  cols(0) <= '0' when cols_counter = "00" else 'Z';
-  cols(1) <= '0' when cols_counter = "01" else 'Z';
-  cols(2) <= '0' when cols_counter = "10" else 'Z';
-  cols(3) <= '0' when cols_counter = "11" else 'Z';
+  cols(0) <= '0' when cols_counter = "00" else '1';
+  cols(1) <= '0' when cols_counter = "01" else '1';
+  cols(2) <= '0' when cols_counter = "10" else '1';
+  cols(3) <= '0' when cols_counter = "11" else '1';
 
   keystate <= internal_keys when cols_counter = "00";
 
-  process(s1_clk)
+  process(keypad_clk)
   begin
-    if(rising_edge(s1_clk)) then 
+    if(rising_edge(keypad_clk)) then 
 
       case cols_counter is
         when "00" =>
@@ -78,6 +80,27 @@ proc_registers : process (s1_clk, s1_reset) begin
     end if;
   end if;
 end process proc_registers;
+
+
+process (s1_clk) begin
+  if(rising_edge(s1_clk)) then
+  
+    if keypad_clk_count < 250000 then 
+		keypad_clk <= '1';
+	 else
+		keypad_clk <= '0';
+	 end if;
+  
+  
+    keypad_clk_count <= keypad_clk_count + 1;
+  
+  
+  end if;
+ 
+
+
+end process;
+
 
 
         end;
