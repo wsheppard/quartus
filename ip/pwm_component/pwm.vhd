180952e2738f4341d78df3432300efe6655a84d9
DaveTempleman <dave.templeman@stfc.ac.uk>
2013-04-02 12:51:33 +0200

    updateded header and made synchronous

--- ip/pwm_component/pwm.vhd
+++ ip/pwm_component/pwm.vhd
@@ -1,22 +1,15 @@
 --------------------------------------------------------------------------------
--- Company:         STFC
--- Engineer:    	Dave Templeman    
+-- Project:         ECD Robot Arm
+-- Engineer:        Dave Templeman    
 --
--- Create Date:     Thursday 27st Septempber 2012
--- Design Name:     lcd
+-- Create Date:     Thursday 1st December 2012
+-- Design Name:     Qsys Robot
 -- Component Name:  pwm
--- Target Device:   Cyclone III EP3C120F780C7
+-- Target Device:   Cyclone III EP3C16F484
 -- Tool versions:   Quartus II 11.0 (Service Pack 1)
 -- Description:     
 --  Control PWM
 -- 
---  
--- Dependencies:
---  
---  
--- Revision:
---  Version 0.1 - file created
---  Version 1.0
 --  
 -- Additional Comments:
 --  Clock frequency = 50MHz (20ns)
@@ -30,54 +23,40 @@ entity pwm is
 		clk         : in    std_logic;
 		rst         : in    std_logic;
 		pwm_on_in   : in    std_logic_vector(31 downto 0);
-      pwm_out     : out   std_logic              
+                pwm_out     : out   std_logic              
     );
 end entity pwm;
 
-architecture fsm of pwm is
-
--- Build an enumerated type for the state machine
-type state_type is(
-		pulse_on,       -- pusle starts
-        pulse_off       -- off period of pulse
-	);
-
--- Declare current state signal
-signal current_state : state_type;
+architecture rtl of pwm is
 
 -- Declare internal signals
 signal num_clks     : unsigned(31 downto 0) := x"00000000"; --1073741824
-signal pwm_width    : unsigned(31 downto 0) := x"0000C350";     -- Default to 1ms=50000
-signal pulse_period : unsigned(31 downto 0) := x"000F423F"  ; -- Wait > 20ms 1000000
-
-
--- Quartus II Synthesis Attributes
-attribute syn_encoding : string;
-attribute syn_encoding of state_type : type is "safe";
+signal pwm_width    : unsigned(31 downto 0) := x"0000C350"; --Default to 1ms=50000
+signal pulse_period : unsigned(31 downto 0) := x"000F423F"; --Wait > 20ms 1000000
 
 
 begin
+pulse_period <= x"000F423F";        --20ms period
+pwm_width <= unsigned(pwm_on_in);   
 
-	pwm_out <= '1' when current_state = pulse_on else '0';
-		
-	pwm_width <= unsigned(pwm_on_in);
-		
-	current_state <= pulse_on when (num_clks < pwm_width) else pulse_off;
-		
-	-- This process keeps the num_clks going
-	process(clk)
+	process(clk, rst)
 		begin
-			if(rising_edge(clk)) then
-				if(num_clks >= pulse_period) then
-					num_clks <= x"00000000";
+			if(rst = '0') then
+                num_clks <= (others => '0');
+                pwm_out <= '0';
+            elsif(rising_edge(clk)) then
+                if (num_clks < pwm_width) then
+                    pwm_out <= '1';
+                    else 
+                    pwm_out <= '0';
+                end if;
+				
+                if(num_clks >= pulse_period) then
+					num_clks <= (others => '0');
 				else
 					num_clks <= num_clks + 1;
 				end if;
-			
 			end if;
-		
 	end process;
 
-
-end fsm;
-						
+end rtl;
